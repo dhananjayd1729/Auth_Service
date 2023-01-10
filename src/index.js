ba309b02b85a7abddac80ccf35eac9bb4475346f
Dhananjay <dhananjay@pop-os.localdomain>
2023-01-10 19:18:52 +0530

    Added creation & verification of jwt token's logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("../src/config/serverConfig");
 
-// const UserRepository = require("./repository/user-repository");
+const UserService = require("./services/user-service");
 
 const apiRoutes = require("./routes/index");
 
@@ -16,8 +16,16 @@ const prepareAndStartServer = () => {
   app.listen(PORT, async () => {
     console.log(`Server started on port : ${PORT}`);
 
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
+    const service = new UserService();
+    // const response = await service.createToken({
+    //   email: "xyz@admin.com",
+    //   id: 1,
+    // });
+    // console.log("new token is ", response);
+
+    // const token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inh5ekBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjczMzU4MzU5LCJleHAiOjE2NzMzNTgzODl9.C_r-3IsoYszVtW9amAF9UD-R8eF_xCtTx8CUuUZTmzQ";
+    // const response = service.verifyToken(token);
     // console.log(response);
   });
 };
